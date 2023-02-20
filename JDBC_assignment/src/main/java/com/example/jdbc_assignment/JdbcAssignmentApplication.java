50a5209d96705e6c2b5d933e654c1b7bf6f42011
Frederik Darling <frederikdarling@live.dk>
2023-02-20 15:48:51 +0100

    initial commit of JDBC project

---
+++ JDBC_assignment/src/main/java/com/example/jdbc_assignment/JdbcAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbc_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcAssignmentApplication.class, args);
+    }
+
+}
